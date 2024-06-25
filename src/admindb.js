e73259c836490f8d86a6bb0f999da602b76ed956
Hakan Özdemir <hakan07ant@gmail.com>
2024-06-25 11:58:10 +0300

    .

--- src/admindb.js
+++ src/admindb.js
@@ -1,23 +1,26 @@
-const { connect } = require('./db-connection');
-const mongoose = require('mongoose');
-const url = "mongodb+srv://user:pwd@wedlock-cluster.da3ng.mongodb.net/admindb";
+const { connect } = require("./db-connection");
+const mongoose = require("mongoose");
+const url = "mongodb://localhost:27017/admindb";
 let db;
-const tenantSchema = new mongoose.Schema({
+const tenantSchema = new mongoose.Schema(
+  {
     id: Number,
-    name: String
-}, { timestamps: true })
+    name: String,
+  },
+  { timestamps: true }
+);
 
 const tenantModel = mongoose.model("tenants", tenantSchema);
 
 const getDb = async () => {
-    return db ? db : await connect(url)
-}
+  return db ? db : await connect(url);
+};
 
 const getTenantModel = async () => {
-    const adminDb = await getDb();
-    return adminDb.model("tenants", tenantSchema)
-}
+  const adminDb = await getDb();
+  return adminDb.model("tenants", tenantSchema);
+};
 
 module.exports = {
-    getTenantModel
-}
+  getTenantModel,
+};
